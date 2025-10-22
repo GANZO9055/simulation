4fe3158739482a0e3e30f655c6f34bad0af9231e
GANZO9055 <kosta.maslihov@gmail.com>
2025-10-22 19:16:08 +0300

    refactor(InsertEntity): adding functions

--- src/main/java/ru/simulation/Simulation.java
+++ src/main/java/ru/simulation/Simulation.java
@@ -1,12 +1,51 @@
 package ru.simulation;
 
+import ru.simulation.action.Action;
+import ru.simulation.action.InitializationAction;
+import ru.simulation.action.MoveAllCreatureAction;
+import ru.simulation.map.MapEntity;
 import ru.simulation.rendering.MapConsoleRender;
 import ru.simulation.rendering.Render;
 
-public class Simulation {
-    public static void main(String[] args) {
-        Render consoleRender = new MapConsoleRender();
+import java.util.List;
 
-        consoleRender.render();
+public class Simulation {
+    private final MapEntity map;
+    private int counterOfMoves;
+    private final Render render;
+    private final List<Action> actions;
+
+    public Simulation() {
+        this.map = new MapEntity();
+        this.counterOfMoves = 0;
+        this.render = new MapConsoleRender();
+        this.actions = List.of(
+                new InitializationAction(),
+                new MoveAllCreatureAction()
+        );
+    }
+
+    public void initialization() {
+        actions.get(0).perform(map);
+        render.render(map);
+    }
+
+    public void nextTurn() {
+        counterOfMoves++;
+        actions.get(1).perform(map);
+        render.render(map);
+    }
+
+    public void startSimulation(int value) {
+        while(value != 0) {
+            counterOfMoves++;
+            actions.get(1).perform(map);
+            render.render(map);
+            value--;
+        }
+    }
+
+    public int getCounterOfMoves() {
+        return counterOfMoves;
     }
 }
