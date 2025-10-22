990de8aa607e11efcbf2efd222b2ce416c008a84
GANZO9055 <kosta.maslihov@gmail.com>
2025-10-22 19:15:16 +0300

    refactor(InsertEntity): changing probability

--- src/main/java/ru/simulation/map/InsertEntity.java
+++ src/main/java/ru/simulation/map/InsertEntity.java
@@ -10,13 +10,13 @@ import java.util.Random;
 public class InsertEntity {
     public Entity entityGeneration(Coordinate coordinate) {
         Random random = new Random();
-        int value = random.nextInt(10);
+        int value = random.nextInt(20);
         return switch (value) {
-            case 0 -> new Grass();
-            case 1 -> new Rock();
-            case 2 -> new Tree();
-            case 3 -> new Herbivore(coordinate);
-            case 4 -> new Predator(coordinate);
+            case 0, 1 -> new Grass();
+            case 2, 3 -> new Rock();
+            case 4, 5 -> new Tree();
+            case 6, 7 -> new Herbivore(coordinate);
+            case 8 -> new Predator(coordinate);
             default -> null;
         };
     }
