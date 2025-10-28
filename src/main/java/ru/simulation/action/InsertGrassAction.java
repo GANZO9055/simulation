ebcabd0ca6bc4cdd35b2b021b4e0b538e9dbf735
GANZO9055 <kosta.maslihov@gmail.com>
2025-10-28 21:43:04 +0300

    refactor(InsertGrassAction, InsertHerbivoreAction): adding function counting, inserting

--- src/main/java/ru/simulation/action/InsertGrassAction.java
+++ src/main/java/ru/simulation/action/InsertGrassAction.java
@@ -1,18 +1,51 @@
 package ru.simulation.action;
 
+import ru.simulation.entity.Entity;
+import ru.simulation.entity.Grass;
+import ru.simulation.game_map.Coordinate;
+import ru.simulation.game_map.EntityFactory;
+import ru.simulation.game_map.GenerationNumber;
 import ru.simulation.game_map.WorldMap;
+
+import java.util.Map;
 
 public class InsertGrassAction implements Action {
     @Override
     public void perform(WorldMap map) {
-        if (!checkQuantityGrass(map)) {
+        if (!checkQuantityGrass(map.getEntities())) {
             return;
         }
-        map.insertGrass();
+        insertGrass(map);
     }
 
-    private boolean checkQuantityGrass(WorldMap map) {
-        map.counterGrassAndHerbivore();
-        return map.getCountGrass() <= 2;
+    private boolean checkQuantityGrass(Map<Coordinate, Entity> entityMap) {
+        int number = counterGrass(entityMap);
+        return number <= 2;
+    }
+
+    private int counterGrass(Map<Coordinate, Entity> entityMap) {
+        int countGrass = 0;
+        for(var entity : entityMap.entrySet()) {
+           if (entity instanceof Grass) {
+               countGrass++;
+           }
+        }
+        return countGrass;
+    }
+
+    private void insertGrass(WorldMap map) {
+        GenerationNumber generationNumber = new GenerationNumber();
+        EntityFactory entityFactory = new EntityFactory();
+        int quantity = generationNumber.getNumber((map.getWidth() * map.getHeight()) / 10);
+        while (quantity != 0) {
+            int x = generationNumber.getNumber(map.getWidth());
+            int y = generationNumber.getNumber(map.getHeight());
+            Coordinate newCoordinate = new Coordinate(x, y);
+            Entity newEntity = entityFactory.grassGeneration();
+            if (map.getEntity(newCoordinate) == null) {
+                map.addEntity(newCoordinate, newEntity);
+                quantity--;
+            }
+        }
     }
 }
--- src/main/java/ru/simulation/action/InsertHerbivoreAction.java
+++ src/main/java/ru/simulation/action/InsertHerbivoreAction.java
@@ -1,18 +1,51 @@
 package ru.simulation.action;
 
+import ru.simulation.entity.Entity;
+import ru.simulation.entity.creature.Herbivore;
+import ru.simulation.game_map.Coordinate;
+import ru.simulation.game_map.EntityFactory;
+import ru.simulation.game_map.GenerationNumber;
 import ru.simulation.game_map.WorldMap;
+
+import java.util.Map;
 
 public class InsertHerbivoreAction implements Action {
     @Override
     public void perform(WorldMap map) {
-        if (!checkQuantityHerbivore(map)) {
+        if (!checkQuantityHerbivore(map.getEntities())) {
             return;
         }
-        map.insertHerbivore();
+        insertHerbivore(map);
     }
 
-    private boolean checkQuantityHerbivore(WorldMap map) {
-        map.counterGrassAndHerbivore();
-        return map.getCountHerbivore() <= 2;
+    private boolean checkQuantityHerbivore(Map<Coordinate, Entity> entityMap) {
+        int number = counterHerbivore(entityMap);
+        return number <= 2;
+    }
+
+    private int counterHerbivore(Map<Coordinate, Entity> entityMap) {
+        int countHerbivore = 0;
+        for(var entity : entityMap.entrySet()) {
+            if (entity instanceof Herbivore) {
+                countHerbivore++;
+            }
+        }
+        return countHerbivore;
+    }
+
+    private void insertHerbivore(WorldMap map) {
+        GenerationNumber generationNumber = new GenerationNumber();
+        EntityFactory entityFactory = new EntityFactory();
+        int quantity = generationNumber.getNumber((map.getWidth() * map.getHeight()) / 10);
+        while (quantity != 0) {
+            int x = generationNumber.getNumber(map.getWidth());
+            int y = generationNumber.getNumber(map.getHeight());
+            Coordinate newCoordinate = new Coordinate(x, y);
+            Entity newEntity = entityFactory.herbivoreGeneration(newCoordinate);
+            if (map.getEntity(newCoordinate) == null) {
+                map.addEntity(newCoordinate, newEntity);
+                quantity--;
+            }
+        }
     }
 }
